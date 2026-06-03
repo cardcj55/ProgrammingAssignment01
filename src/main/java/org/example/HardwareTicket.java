b3fb4fd8696d530bbade85aa5b38e5603243125b
Chris <cardcj@farmingdale.edu>
2026-06-03 16:18:44 -0400

    Implement urgencyScore calculation logic

--- src/main/java/org/example/HardwareTicket.java
+++ src/main/java/org/example/HardwareTicket.java
@@ -29,7 +29,10 @@ public class HardwareTicket extends Ticket {
     // -----------------------------------------
     @Override
     public int urgencyScore() {
-        // TODO #2
-        return -1;
+        int score = getPriority() * 10 + getDaysOpen() * 2;
+        score += labCritical ? 20 : 0;
+        score += Math.min(affectedUsers, 30);
+        score += "Printer".equalsIgnoreCase(deviceType) ? 5 : 0;
+        return score;
     }
 }
